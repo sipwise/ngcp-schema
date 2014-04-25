8027b3af39ae72b7cd2d434e99d000a324b377a9
Andreas Granig <agranig@sipwise.com>
2014-04-25 14:20:05 +0200

    MT#6789 Add email template schema.

---
+++ lib/NGCP/Schema/Result/email_templates.pm
@@ -0,0 +1,53 @@
+package NGCP::Schema::Result::email_templates;
+use Scalar::Util qw(blessed);
+use parent 'DBIx::Class::Core';
+
+our $VERSION = '2.007';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "Helper::Row::ToJSON");
+
+__PACKAGE__->table("billing.email_templates");
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "reseller_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 0,
+  },
+  "name",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+  "from_email",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+  "subject",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+  "body",
+  { data_type => "mediumtext", is_nullable => 0 },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+__PACKAGE__->belongs_to(
+  "reseller",
+  "NGCP::Schema::Result::resellers",
+  { id => "reseller_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+sub TO_JSON {
+    my ($self) = @_;
+    return {
+        map { blessed($_) && $_->isa('DateTime') ? $_->datetime : $_ } %{ $self->next::method }
+    };
+}
+
+1;
