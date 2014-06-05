9a255c41b5c888dedb9473f14209e3eb3b61eeb7
Andreas Granig <agranig@sipwise.com>
2014-06-05 12:10:00 +0200

    MT#5879 Cleanup invoice tables and add gpp contact
    Drop unnecessary invoice template tables.
    Provide proper columns in invoice and invoice templates table.
    Add general purpose columns to contacts.

--- lib/NGCP/Schema/Result/invoices.pm
+++ lib/NGCP/Schema/Result/invoices.pm
@@ -16,33 +16,25 @@ __PACKAGE__->add_columns(
     is_auto_increment => 1,
     is_nullable => 0,
   },
-  "year",
-  { data_type => "smallint", extra => { unsigned => 1 }, is_nullable => 0 },
-  "month",
-  { data_type => "tinyint", extra => { unsigned => 1 }, is_nullable => 0 },
   "serial",
+  { data_type => "varchar", is_nullable => 0, size => 32 },
+  "period_start",
   { data_type => "integer", extra => { unsigned => 1 }, is_nullable => 0 },
+  "period_end",
+  { data_type => "integer", extra => { unsigned => 1 }, is_nullable => 0 },
+  "amount_net",
+  { data_type => "double precision", default_value => 0, is_nullable => 0 },
+  "amount_vat",
+  { data_type => "double precision", default_value => 0, is_nullable => 0 },
+  "amount_total",
+  { data_type => "double precision", default_value => 0, is_nullable => 0 },
   "data",
   { data_type => "blob", is_nullable => 1 },
 );
 
 __PACKAGE__->set_primary_key("id");
 
-__PACKAGE__->add_unique_constraint("yms_idx", ["year", "month", "serial"]);
-
-__PACKAGE__->has_many(
-  "contract_balances",
-  "NGCP::Schema::Result::contract_balances",
-  { "foreign.invoice_id" => "self.id" },
-  { cascade_copy => 0, cascade_delete => 0 },
-);
-
-__PACKAGE__->has_many(
-  "orders",
-  "NGCP::Schema::Result::orders",
-  { "foreign.invoice_id" => "self.id" },
-  { cascade_copy => 0, cascade_delete => 0 },
-);
+__PACKAGE__->add_unique_constraint("serial_idx", ["serial"]);
 
 sub TO_JSON {
     my ($self) = @_;
@@ -52,90 +44,3 @@ sub TO_JSON {
 }
 
 1;
-__END__
-
-=encoding UTF-8
-
-=head1 NAME
-
-NGCP::Schema::Result::invoices
-
-=head1 COMPONENTS LOADED
-
-=over 4
-
-=item * L<DBIx::Class::InflateColumn::DateTime>
-
-=item * L<DBIx::Class::Helper::Row::ToJSON>
-
-=back
-
-=head1 TABLE: C<billing.invoices>
-
-=head1 ACCESSORS
-
-=head2 id
-
-  data_type: 'integer'
-  extra: {unsigned => 1}
-  is_auto_increment: 1
-  is_nullable: 0
-
-=head2 year
-
-  data_type: 'smallint'
-  extra: {unsigned => 1}
-  is_nullable: 0
-
-=head2 month
-
-  data_type: 'tinyint'
-  extra: {unsigned => 1}
-  is_nullable: 0
-
-=head2 serial
-
-  data_type: 'integer'
-  extra: {unsigned => 1}
-  is_nullable: 0
-
-=head2 data
-
-  data_type: 'blob'
-  is_nullable: 1
-
-=head1 PRIMARY KEY
-
-=over 4
-
-=item * L</id>
-
-=back
-
-=head1 UNIQUE CONSTRAINTS
-
-=head2 C<yms_idx>
-
-=over 4
-
-=item * L</year>
-
-=item * L</month>
-
-=item * L</serial>
-
-=back
-
-=head1 RELATIONS
-
-=head2 contract_balances
-
-Type: has_many
-
-Related object: L<NGCP::Schema::Result::contract_balances>
-
-=head2 orders
-
-Type: has_many
-
-Related object: L<NGCP::Schema::Result::orders>
