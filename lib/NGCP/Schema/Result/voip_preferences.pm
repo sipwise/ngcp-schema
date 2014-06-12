0f7460d61b75454c9d5dba526e6e32911762d52c
Andreas Granig <agranig@sipwise.com>
2014-06-12 20:42:57 +0200

    MT#7471 Add contract preferences schema.

--- lib/NGCP/Schema/Result/voip_preferences.pm
+++ lib/NGCP/Schema/Result/voip_preferences.pm
@@ -101,6 +101,13 @@ __PACKAGE__->has_many(
   { cascade_copy => 0, cascade_delete => 0 },
 );
 
+__PACKAGE__->has_many(
+  "voip_contract_preferences",
+  "NGCP::Schema::Result::voip_contract_preferences",
+  { "foreign.attribute_id" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
 sub TO_JSON {
     my ($self) = @_;
     return {
--- lib/NGCP/Schema/Result/voip_preferences_enum.pm
+++ lib/NGCP/Schema/Result/voip_preferences_enum.pm
@@ -28,6 +28,8 @@ __PACKAGE__->add_columns(
   { data_type => "tinyint", default_value => 0, is_nullable => 1 },
   "peer_pref",
   { data_type => "tinyint", default_value => 0, is_nullable => 1 },
+  "contract_pref",
+  { data_type => "tinyint", default_value => 0, is_nullable => 1 },
   "default_val",
   { data_type => "tinyint", is_nullable => 1 },
 );
