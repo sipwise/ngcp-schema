722fd5ed72a05b84d168c04e4cca36c7e512f10b
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2014-10-10 21:55:55 +0300

    MT#9177 Schema for provisioning with centralized table. Draft.

---
+++ lib/NGCP/Schema/Result/autoprov_sync.pm
@@ -0,0 +1,102 @@
+package NGCP::Schema::Result::autoprov_sync_parameters;
+use Scalar::Util qw(blessed);
+use parent 'DBIx::Class::Core';
+
+our $VERSION = '2.007';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "Helper::Row::ToJSON");
+
+__PACKAGE__->table("provisioning.autoprov_sync_parameters");
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "device_id",
+  { data_type => "integer", extra => { unsigned => 1 }, is_nullable => 0 },
+  "parameter_id",
+  { data_type => "integer", extra => { unsigned => 1 }, is_nullable => 0 },
+  "parameter_value",
+  { data_type => "varchar", size => 255, is_nullable => 0 },
+);
+
+__PACKAGE__->belongs_to(
+  "device",
+  "NGCP::Schema::Result::autoprov_devices",
+  { id => "device_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  }
+);
+__PACKAGE__->belongs_to(
+  "autoprov_sync_parameter",
+  "NGCP::Schema::Result::autoprov_sync_parameter",
+  { id => "parameter_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  }
+);
+
+
+__PACKAGE__->set_primary_key("id");
+
+sub TO_JSON {
+    my ($self) = @_;
+    return {
+        map { blessed($_) && $_->isa('DateTime') ? $_->datetime : $_ } %{ $self->next::method }
+    };
+}
+
+1;
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+NGCP::Schema::provisioning::Result::autoprov_sync_cisco
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=item * L<DBIx::Class::Helper::Row::ToJSON>
+
+=back
+
+=head1 TABLE: C<autoprov_sync_cisco>
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 device_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
---
+++ lib/NGCP/Schema/Result/autoprov_sync_parameters.pm
@@ -0,0 +1,100 @@
+package NGCP::Schema::Result::autoprov_sync_parameters;
+use Scalar::Util qw(blessed);
+use parent 'DBIx::Class::Core';
+
+our $VERSION = '2.007';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "Helper::Row::ToJSON");
+
+__PACKAGE__->table("provisioning.autoprov_sync_parameters");
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "sync_type",
+  {
+    data_type => "enum",
+    default_value => "cisco",
+    extra => { list => ["cisco","panasonic","linksys"] },
+    is_nullable => 0,
+  },   
+  "parameter_name",
+  {
+    data_type => "enum",
+    extra => { list => ["sync_uri","sync_method","sync_params","security_handler"] },
+    is_nullable => 1,
+  },
+  "parameter_constraint",
+  { data_type => "varchar", is_nullable => 1, size => 255 },
+);
+
+__PACKAGE__->has_many(
+  "autoprov_sync",
+  "NGCP::Schema::Result::autoprov_sync",
+  { id => "parameter_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  }
+);
+
+
+__PACKAGE__->set_primary_key("id");
+
+sub TO_JSON {
+    my ($self) = @_;
+    return {
+        map { blessed($_) && $_->isa('DateTime') ? $_->datetime : $_ } %{ $self->next::method }
+    };
+}
+
+1;
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+NGCP::Schema::provisioning::Result::autoprov_sync_cisco
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=item * L<DBIx::Class::Helper::Row::ToJSON>
+
+=back
+
+=head1 TABLE: C<autoprov_sync_cisco>
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 device_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
