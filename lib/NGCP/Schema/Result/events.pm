c32fdca705110a763617d1bedd59e0d717557949
Andreas Granig <agranig@sipwise.com>
2015-04-01 14:39:28 +0200

    MT#12385 Add reseller_id to EDR head.
    Change-Id: I2a6845f0a6a7b4d2adc0f7493f1a1138b977795c

--- lib/NGCP/Schema/Result/events.pm
+++ lib/NGCP/Schema/Result/events.pm
@@ -24,6 +24,8 @@ __PACKAGE__->add_columns(
   { data_type => "varchar", is_nullable => 0, size => 255 },
   "subscriber_id",
   { data_type => "integer", extra => { unsigned => 1 }, is_nullable => 0 },
+  "reseller_id",
+  { data_type => "integer", extra => { unsigned => 1 }, is_nullable => 0 },
   "old_status",
   { data_type => "varchar", is_nullable => 1, size => 255 },
   "new_status",
