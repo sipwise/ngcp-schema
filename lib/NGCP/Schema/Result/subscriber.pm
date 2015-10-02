3517a0713dc42c2f55e69119d54dfaad23efa9cd
Rene Krenn <rkrenn@sipwise.com>
2015-10-02 10:51:02 +0200

    MT#15477 topup_log resultset WIP
    Change-Id: Ic97aad9ea2c75e67e93a4ad6e48e55395786511a

--- lib/NGCP/Schema/Result/subscriber.pm
+++ lib/NGCP/Schema/Result/subscriber.pm
@@ -61,6 +61,13 @@ __PACKAGE__->has_many(
   { cascade_copy => 0, cascade_delete => 0 },
 );
 
+__PACKAGE__->has_many(
+  "topup_log",
+  "NGCP::Schema::Result::topup_log",
+  { 'foreign.subscriber_id' => 'self.id' },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
 sub TO_JSON {
     my ($self) = @_;
     return {
