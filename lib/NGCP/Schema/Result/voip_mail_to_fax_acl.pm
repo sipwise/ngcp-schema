54df813b87b093bf993d0d786a2f1cd6780ed1d1
Kirill Solomko <ksolomko@sipwise.com>
2015-10-12 13:11:41 +0200

    MT#14679 new voip_mail_to_fax tables, new voip_fax_journal table
    Change-Id: I2d8d3b26613582a77a211fa469ce08548071f949

---
+++ lib/NGCP/Schema/Result/voip_mail_to_fax_acl.pm
@@ -0,0 +1,139 @@
+package NGCP::Schema::Result::voip_mail_to_fax_acl;
+use Scalar::Util qw(blessed);
+use parent 'DBIx::Class::Core';
+
+our $VERSION = '2.007';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "Helper::Row::ToJSON");
+
+__PACKAGE__->table("provisioning.voip_mail_to_fax_acl");
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "subscriber_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 0,
+  },
+  "from_email",
+  { data_type => "varchar", is_nullable => 1, size => 255 },
+  "received_from",
+  { data_type => "varchar", is_nullable => 1, size => 255 },
+  "destination",
+  { data_type => "varchar", is_nullable => 1, size => 255 },
+  "use_regex",
+  { data_type => "integer", is_nullable => 0, default_value => 0 },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+__PACKAGE__->belongs_to(
+  "provisioning_voip_subscriber",
+  "NGCP::Schema::Result::provisioning_voip_subscribers",
+  { id => "subscriber_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+sub TO_JSON {
+    my ($self) = @_;
+    return {
+        map { blessed($_) && $_->isa('DateTime') ? $_->datetime : $_ } %{ $self->next::method }
+    };
+}
+
+1;
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+NGCP::Schema::Result::voip_mail_to_fax_acl
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=item * L<DBIx::Class::Helper::Row::ToJSON>
+
+=back
+
+=head1 TABLE: C<provisioning.voip_mail_to_fax_acl>
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 subscriber_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 from_email
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 255
+
+=head2 received_from
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 255
+
+=head2 destination
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 255
+
+=head2 use_regex
+
+  data_type: 'integer'
+  is_nullable: 0
+  default_value: 0
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<subdest_idx>
+
+=over 4
+
+=item * L</subscriber_id>
+
+=item * L</destination>
+
+=back
+
+=head1 RELATIONS
+
+=head2 subscriber
+
+Type: belongs_to
+
+Related object: L<NGCP::Schema::Result::provisioning_voip_subscribers>
