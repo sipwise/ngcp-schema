92b4eb34b47fc4e278c00e3c48787d6b352430f9
Kirill Solomko <ksolomko@sipwise.com>
2015-10-12 13:11:41 +0200

    MT#14679 - add voip_mail_to_fax tables and relation in provisioning voip_subscribers
    Change-Id: I2d8d3b26613582a77a211fa469ce08548071f949

---
+++ lib/NGCP/Schema/Result/voip_mail_to_fax_preferences.pm
@@ -0,0 +1,150 @@
+package NGCP::Schema::Result::voip_mail_to_fax_preferences;
+use Scalar::Util qw(blessed);
+use parent 'DBIx::Class::Core';
+
+our $VERSION = '2.007';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "Helper::Row::ToJSON");
+
+__PACKAGE__->table("provisioning.voip_mail_to_fax_preferences");
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "subscriber_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 0,
+  },
+  "active",
+  { data_type => "tinyint", default_value => 0, is_nullable => 0 },
+  "secret_key",
+  { data_type => "varchar", is_nullable => 1 },
+  "last_secret_key_modify",
+  {
+    data_type => 'timestamp',
+    datetime_undef_if_invalid => 1,
+    default_value => '0000-00-00 00:00:00',
+    is_nullable => 0,
+  },
+  "secret_key_renew",
+  {
+    data_type => "enum",
+    default_value => "never",
+    extra => { list => ["never", "daily", "weekly", "monthly"] },
+    is_nullable => 0,
+  },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+__PACKAGE__->add_unique_constraint("subscriberid_idx", ["subscriber_id"]);
+
+__PACKAGE__->belongs_to(
+  "subscriber",
+  "NGCP::Schema::Result::provisioning_voip_subscribers",
+  { id => "subscriber_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+sub TO_JSON {
+    my ($self) = @_;
+    return {
+        map { blessed($_) && $_->isa('DateTime') ? $_->datetime : $_ } %{ $self->next::method }
+    };
+}
+
+1;
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+NGCP::Schema::Result::voip_mail_to_fax_preferences
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=item * L<DBIx::Class::Helper::Row::ToJSON>
+
+=back
+
+=head1 TABLE: C<provisioning.voip_mail_to_fax_preferences>
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 subscriber_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 active
+
+  data_type: 'tinyint'
+  default_value: 0
+  is_nullable: 0
+
+=head2 secret_key
+
+  data_type: 'varchar'
+  is_nullable: 1
+
+=head2 last_secret_key_modify
+
+    data_type: 'timestamp',
+    datetime_undef_if_invalid: 1
+    default_value: '0000-00-00 00:00:00'
+    is_nullable: 0
+
+=head2 secret_key_renew
+
+  data_type: 'enum'
+  default_value: 'never'
+  extra: {list => ["never","daily","weekly","monthly"]}
+  is_nullable: 0
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<subscriberid_idx>
+
+=over 4
+
+=item * L</subscriber_id>
+
+=back
+
+=head1 RELATIONS
+
+=head2 subscriber
+
+Type: belongs_to
+
+Related object: L<NGCP::Schema::Result::provisioning_voip_subscribers>
