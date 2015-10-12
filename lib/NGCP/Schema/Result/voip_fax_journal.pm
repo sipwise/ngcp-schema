028df3452ece921451b3654f6b7d556ddc5488f2
Kirill Solomko <ksolomko@sipwise.com>
2015-10-12 13:11:41 +0200

    MT#14679 new voip_mail_to_fax tables, new voip_fax_journal table
    Change-Id: I2d8d3b26613582a77a211fa469ce08548071f949

---
+++ lib/NGCP/Schema/Result/voip_fax_journal.pm
@@ -0,0 +1,212 @@
+package NGCP::Schema::Result::voip_fax_journal;
+use Scalar::Util qw(blessed);
+use parent 'DBIx::Class::Core';
+
+our $VERSION = '2.007';
+
+__PACKAGE__->load_components(
+    "InflateColumn::DateTime",
+    "Helper::Row::ToJSON",
+    "+NGCP::Schema::InflateColumn::DateTime::EpochString",
+);
+
+__PACKAGE__->table("provisioning.voip_fax_journal");
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "subscriber_id",
+  {
+    data_type => "integer",
+    default_value => 0,
+    extra => { unsigned => 1 },
+    is_nullable => 0,
+  },
+  "the_timestamp",
+  {
+    data_type => "integer",
+    default_value => 0,
+    extra => { unsigned => 1 },
+    is_nullable => 0,
+    inflate_datetime => 'epoch_string',
+  },
+  "duration",
+  {
+    data_type => "integer",
+    default_value => 0,
+    extra => { unsigned => 1 },
+    is_nullable => 0,
+  },
+  "direction",
+  {
+    data_type => "enum",
+    extra => { list => ["in", "out", "mtf"] },
+    is_nullable => 0,
+  },
+  "peer_number",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
+  "peer_name",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
+  "pages",
+  {
+    data_type => "integer",
+    default_value => 0,
+    extra => { unsigned => 1 },
+    is_nullable => 0,
+  },
+  "reason",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+  "status",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
+  "signal_rate",
+  {
+    data_type => "integer",
+    default_value => 0,
+    extra => { unsigned => 1 },
+    is_nullable => 0,
+  },
+  "quality",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
+  "filename",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
+);
+__PACKAGE__->belongs_to(
+  "provisioning_voip_subscriber",
+  "NGCP::Schema::Result::provisioning_voip_subscribers",
+  { 'foreign.id' => 'self.subscriber_id' },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+sub TO_JSON {
+    my ($self) = @_;
+    return {
+        map { blessed($_) && $_->isa('DateTime') ? $_->datetime : $_ } %{ $self->next::method }
+    };
+}
+
+1;
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+NGCP::Schema::Result::voip_fax_journal
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=item * L<DBIx::Class::Helper::Row::ToJSON>
+
+=back
+
+=head1 TABLE: C<provisioning.voip_fax_journal>
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 subscriber_id
+
+  data_type: 'integer'
+  default_value: 0
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head2 the_timestamp
+
+  data_type: 'integer'
+  default_value: 0
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head2 duration
+
+  data_type: 'integer'
+  default_value: 0
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head2 direction
+
+  data_type: 'enum'
+  extra: {list => ["in","out","mtf"]}
+  is_nullable: 0
+
+=head2 peer_number
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 255
+
+=head2 peer_name
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 255
+
+=head2 pages
+
+  data_type: 'integer'
+  default_value: 0
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head2 reason
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 255
+
+=head2 status
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 255
+
+=head2 signal_rate
+
+  data_type: 'integer'
+  default_value: 0
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head2 quality
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 255
+
+=head2 filename
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 255
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
