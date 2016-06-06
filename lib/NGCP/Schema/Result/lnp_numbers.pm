14c8888bfcc5b7d9c8f2a9a294c1c994b03eefac
Rene Krenn <rkrenn@sipwise.com>
2016-06-06 17:31:56 +0200

    MT#20023 MT#20027 lnp numbers unique constraint and cascade delete restriction
    Change-Id: I8262c7c4390680e455bb9aef330e245f639ebf1f

--- lib/NGCP/Schema/Result/lnp_numbers.pm
+++ lib/NGCP/Schema/Result/lnp_numbers.pm
@@ -43,11 +43,13 @@ __PACKAGE__->add_columns(
 
 __PACKAGE__->set_primary_key("id");
 
+__PACKAGE__->add_unique_constraint("l_n_lnpproidnumber_idx", ["lnp_provider_id", "number"]);
+
 __PACKAGE__->belongs_to(
   "lnp_provider",
   "NGCP::Schema::Result::lnp_providers",
   { id => "lnp_provider_id" },
-  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+  { is_deferrable => 1, on_delete => "RESTRICT", on_update => "CASCADE" },
 );
 
 sub TO_JSON {
