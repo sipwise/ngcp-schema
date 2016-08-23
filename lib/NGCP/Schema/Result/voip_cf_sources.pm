6cb5a8467e54cac3b64d71e633460df9e8e6ef9d
Gerhard Jungwirth <gjungwirth@sipwise.com>
2016-08-23 14:03:47 +0200

    TT#2366 new tables voip_cf_source* for anumber cfs
    callforwards based on the caller number
    
    Change-Id: If961a329b6cd9564f73faa7e1d24d0f0f76426be

---
+++ lib/NGCP/Schema/Result/voip_cf_sources.pm
@@ -0,0 +1,116 @@
+package NGCP::Schema::Result::voip_cf_sources;
+use Scalar::Util qw(blessed);
+use parent 'DBIx::Class::Core';
+
+our $VERSION = '2.007';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "Helper::Row::ToJSON");
+
+__PACKAGE__->table("provisioning.voip_cf_sources");
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "source_set_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 0,
+  },
+  "source",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+__PACKAGE__->belongs_to(
+  "source_set",
+  "NGCP::Schema::Result::voip_cf_source_sets",
+  { id => "source_set_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+sub TO_JSON {
+    my ($self) = @_;
+    return {
+        map { blessed($_) && $_->isa('DateTime') ? $_->datetime : $_ } %{ $self->next::method }
+    };
+}
+
+1;
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+NGCP::Schema::Result::voip_cf_sources
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=item * L<DBIx::Class::Helper::Row::ToJSON>
+
+=back
+
+=head1 TABLE: C<provisioning.voip_cf_sources>
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 source_set_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 source
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 255
+
+=head2 priority
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_nullable: 1
+
+=head2 timeout
+
+  data_type: 'integer'
+  default_value: 300
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=head1 RELATIONS
+
+=head2 source_set
+
+Type: belongs_to
+
+Related object: L<NGCP::Schema::Result::voip_cf_source_sets>
