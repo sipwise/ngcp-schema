2befbe0d904a3d033226b2ba42fe17824259bacd
Rene Krenn <rkrenn@sipwise.com>
2016-12-01 07:02:04 +0100

    TT#6735 TT#7030 TT#5850 accountin.events entity-attribute-value models WIP
    Change-Id: I5b336f291a2c449ecd1f678ab7405a0bde4c318f

---
+++ lib/NGCP/Schema/Result/events_tag.pm
@@ -0,0 +1,53 @@
+package NGCP::Schema::Result::events_tag;
+
+use Scalar::Util qw(blessed);
+use parent 'DBIx::Class::Core';
+
+our $VERSION = '2.007';
+
+__PACKAGE__->load_components(
+    "InflateColumn::DateTime",
+    "Helper::Row::ToJSON",
+);
+
+__PACKAGE__->table("accounting.events_tag");
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "type",
+  {
+    data_type => "enum",
+    extra => { list => [
+    'primary_number_cc',
+    'primary_number_ac',
+    'primary_number_sn',
+    'pilot_primary_number_cc',
+    'pilot_primary_number_ac',
+    'pilot_primary_number_sn',
+    'subscriber_profile_name',
+    'subscriber_profile_set_name',
+    'pilot_subscriber_profile_name',
+    'pilot_subscriber_profile_set_name',
+    ] },
+    is_nullable => 0,
+  },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+__PACKAGE__->add_unique_constraint("etc_type_idx", ["type"]);
+
+sub TO_JSON {
+    my ($self) = @_;
+    return {
+        map { blessed($_) && $_->isa('DateTime') ? $_->datetime : $_ } %{ $self->next::method }
+    };
+}
+
+1;
