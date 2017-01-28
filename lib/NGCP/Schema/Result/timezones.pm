e91474f88ae252a13c799f676c468c80cfafdd3b
Kirill Solomko <ksolomko@sipwise.com>
2017-01-28 21:34:52 +0100

    TT#5826 add contact.timezone, add timezone views
        * add voip_subscriber_timezone (view)
        * add voip_contract_timezone (view)
        * add voip_reseller_timezone (view)
    
    Change-Id: Ib61df3627b7f8f06753beb0652a297c45cd043ad

---
+++ lib/NGCP/Schema/Result/timezones.pm
@@ -0,0 +1,51 @@
+package NGCP::Schema::Result::timezones;
+use Scalar::Util qw(blessed);
+use base qw/DBIx::Class::Core/;
+
+our $VERSION = '2.007';
+
+__PACKAGE__->load_components(
+  "InflateColumn::DateTime",
+  "Helper::Row::ToJSON",
+  "+NGCP::Schema::InflateColumn::DateTime::EpochMicro",
+);
+__PACKAGE__->table_class('DBIx::Class::ResultSource::View');
+__PACKAGE__->table("timezones");
+
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_nullable => 0,
+  },
+  "name",
+  {
+    data_type => "varchar",
+    size      => 80,
+    is_nullable => 0,
+  },
+);
+
+sub TO_JSON {
+    my ($self) = @_;
+    return {
+        map { blessed($_) && $_->isa('DateTime') ? $_->datetime : $_ } %{ $self->next::method }
+    };
+}
+
+my $timezones_sql = 'SELECT @rc:=@rc+1 as id, t.name FROM (' .
+                    (join " UNION ALL ",
+                        map { sprintf "SELECT '%s' as name", $_ }
+                            DateTime::TimeZone->all_names) .
+                    ') t, (SELECT @rc:=0) as cnt';
+
+__PACKAGE__->set_primary_key("name");
+
+__PACKAGE__->result_source_instance->is_virtual(1);
+
+__PACKAGE__->result_source_instance->view_definition($timezones_sql);
+
+1;
+
