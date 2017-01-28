32bd010c20745246a29a55c66bcd9e2153c02e0d
Kirill Solomko <ksolomko@sipwise.com>
2017-01-28 21:34:52 +0100

    TT#5826 add contact.timezone, add timezone views
        * add voip_subscriber_timezone (view)
        * add voip_contract_timezone (view)
        * add voip_reseller_timezone (view)
    
    Change-Id: Ib61df3627b7f8f06753beb0652a297c45cd043ad

---
+++ lib/NGCP/Schema/Result/reseller_timezone.pm
@@ -0,0 +1,68 @@
+package NGCP::Schema::Result::reseller_timezone;
+use Scalar::Util qw(blessed);
+use base qw/DBIx::Class::Core/;
+
+our $VERSION = '2.007';
+
+__PACKAGE__->load_components(
+  "InflateColumn::DateTime",
+  "Helper::Row::ToJSON",
+  "+NGCP::Schema::InflateColumn::DateTime::EpochMicro",
+);
+__PACKAGE__->table_class('DBIx::Class::ResultSource::View');
+__PACKAGE__->table("reseller_timezone");
+
+
+__PACKAGE__->add_columns(
+  "contact_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_nullable => 1,
+  },
+  "reseller_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_nullable => 0,
+  },
+  "timezone",
+  {
+    data_type => "varchar",
+    size      => 80,
+    is_nullable => 1,
+  },
+);
+
+__PACKAGE__->belongs_to(
+  "reseller",
+  "NGCP::Schema::Result::resellers",
+  { id => "reseller_id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+__PACKAGE__->belongs_to(
+  "contact",
+  "NGCP::Schema::Result::contacts",
+  { id => "contact_id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+sub TO_JSON {
+    my ($self) = @_;
+    return {
+        map { blessed($_) && $_->isa('DateTime') ? $_->datetime : $_ } %{ $self->next::method }
+    };
+}
+
+__PACKAGE__->result_source_instance->is_virtual(1);
+
+__PACKAGE__->result_source_instance->view_definition("
+SELECT rc.id as contact_id, r.id as reseller_id, rc.timezone
+FROM resellers r
+JOIN contracts i ON i.id = r.contract_id
+JOIN contacts rc ON rc.id = i.contact_id
+");
+
+1;
+
