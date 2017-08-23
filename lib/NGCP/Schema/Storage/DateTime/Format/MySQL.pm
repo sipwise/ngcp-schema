bcab27e90cc698ad4135e3f31aab5060d6981f9e
Rene Krenn <rkrenn@sipwise.com>
2017-08-23 17:24:34 +0200

    TT#12311 DateTime::Format::MySQL, safe for leap seconds
    Change-Id: Id65aa4e39fc0e40e9b97c45c6aeca70f52acad6a
    (cherry picked from commit 6dbc911d7f65315f0b467af261fdd720d8c98282)

---
+++ lib/NGCP/Schema/Storage/DateTime/Format/MySQL.pm
@@ -0,0 +1,28 @@
+package NGCP::Schema::Storage::DateTime::Format::MySQL;
+
+use strict;
+
+use vars qw ($VERSION);
+
+$VERSION = '0.06';
+
+use base ("DateTime::Format::MySQL");
+
+sub format_time
+{
+    my ( $self, $dt ) = @_;
+    my ($h,$m,$s) = ($dt->hour(), $dt->minute(), $dt->second());
+    if ($s >= 60) { #Returns the second, from 0..61. The values 60 and 61 are used for leap seconds.
+        $s = 59;
+    }
+    my $sep = ':';
+    return sprintf(
+        '%0.2d%s%0.2d%s%0.2d',
+        $h, $sep,
+        $m, $sep,
+        $s
+    );
+}
+
+
+1;
