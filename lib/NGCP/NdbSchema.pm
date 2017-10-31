8785eec9b0bc4fedc0e7dcb829e2ecb8d4a0743c
Andreas Granig <agranig@sipwise.com>
2017-10-31 11:40:35 +0100

    TT#22828 Add NDB Schema
    Change-Id: I88f2e7e1f682faac25142704fd33e0027f5aa660

---
+++ lib/NGCP/NdbSchema.pm
@@ -0,0 +1,16 @@
+package NGCP::NdbSchema;
+
+use warnings;
+use strict;
+
+use base 'NGCP::Schema';
+
+our $VERSION = '2.007';
+
+sub connection {
+    my ($self) = @_;
+    my $full_config = NGCP::Schema::Config::get_config_hash();
+    $self->SUPER::connection($full_config->{ngcp_ndb_connect_info});
+}
+
+1;
