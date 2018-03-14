ebd72f824d2a52cba98a5074e90fc921f4941e71
Kirill Solomko <ksolomko@sipwise.com>
2018-03-14 12:57:59 +0100

    TT#14662 add phonebook tables schema
        * reseller_phonebook
        * contract_phonebook
        * subscriber_phonebook
    
    Change-Id: Ica074f05b9706837b94e0f9bd7b4f81e9eaca3b0

---
+++ lib/NGCP/Schema/Result/contract_phonebook.pm
@@ -0,0 +1,141 @@
+package NGCP::Schema::Result::contract_phonebook;
+
+use strict;
+use warnings;
+
+use Scalar::Util qw(blessed);
+use parent 'DBIx::Class::Core';
+
+our $VERSION = '2.007';
+
+__PACKAGE__->table("billing.contract_phonebook");
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "contract_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 0,
+  },
+  "name",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+  "number",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+__PACKAGE__->add_unique_constraint("c_number_u_idx", [qw/contract_id number/]);
+
+__PACKAGE__->belongs_to(
+  "contract",
+  "NGCP::Schema::Result::contracts",
+  { id => "contract_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+1;
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+NGCP::Schema::Result::contract_phonebook
+
+=head1 DESCRIPTION
+
+This module is a schema class for the NGCP database table "billing.contract_phonebook".
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=back
+
+=head1 TABLE: C<billing.contract_phonebook>
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 contract_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 name
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 255
+
+=head2 number
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 255
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<c_number_u_idx>
+
+=over 4
+
+=item * L</contract_id>
+
+=item * L</number>
+
+=back
+
+=head1 RELATIONS
+
+=head2 contract
+
+Type: belongs_to
+
+Related object: L<NGCP::Schema::Result::contracts>
+
+=head1 AUTHOR
+
+Sipwise Development Team C<< <support@sipwise.com> >>
+
+=head1 LICENSE
+
+This software is Copyright © 2018 by Sipwise GmbH, Austria.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this package.  If not, see <https://www.gnu.org/licenses/>.
