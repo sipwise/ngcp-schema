0ae728dedebf8d88cd5c953c42108e83bde701e5
Rene Krenn <rkrenn@sipwise.com>
2018-04-12 16:44:08 +0200

    TT#35662 move product_id to billing.contracts
    Change-Id: Ib7a41f6579e24fcd6307d9168bb7b0669bf2a785
    (cherry picked from commit 4500b7de1d55beaeea87fe5e7c9410600c5cf5c9)
    (cherry picked from commit 274ec65072b00e8d4c8e55c55ef8f54c34e5a470)
    (cherry picked from commit 082ee1e2094e7bd51d38610b728018b98a70ed2c)

--- lib/NGCP/Schema/Result/contracts.pm
+++ lib/NGCP/Schema/Result/contracts.pm
@@ -115,6 +115,13 @@ __PACKAGE__->add_columns(
   { data_type => "tinyint", extra => { unsigned => 1 }, default_value => 0, is_nullable => 0 },
   "add_vat",
   { data_type => "tinyint", extra => { unsigned => 1 }, default_value => 0, is_nullable => 0 },
+  "product_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 1,
+  },
 
 );
 
@@ -277,6 +284,25 @@ __PACKAGE__->has_many(
   "NGCP::Schema::Result::topup_logs",
   { 'foreign.contract_id' => 'self.id' },
   { cascade_copy => 0, cascade_delete => 0 },
+);
+
+__PACKAGE__->might_have(
+  "timezone",
+  'NGCP::Schema::Result::contract_timezone',
+  { 'foreign.contract_id' => 'self.id' },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+__PACKAGE__->belongs_to(
+  "product",
+  "NGCP::Schema::Result::products",
+  { id => "product_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "RESTRICT",
+    on_update     => "CASCADE",
+  },
 );
 
 sub TO_JSON {
