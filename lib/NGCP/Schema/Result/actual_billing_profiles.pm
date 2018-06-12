8a6379ab721de461a02494dcfd2c1d399f4bc069
Rene Krenn <rkrenn@sipwise.com>
2018-06-12 16:28:09 +0200

    TT#38402 new billing mappings resultsets/relationships
    new resultsets:
    
    - "billing_mappings_view":
    fast virtual view listing a contract's billing mappings
    according to the legacy table layout. this is used
    by panel UI to present the mappings in the convenient,
    known way.
    
    - "actual_billing_profiles":
    a resultset for joining the fast(er)
    billing.v_actual_billing_profiles it is only used for
    filters involving the (actual) billing profile or its
    properties.
    
    - "contracts_billing_profile_network" and
    "contracts_billing_profile_network_schedule":
    resultsets for the new db tables. added for completeness,
    but not directly used by panel dbix code.
    
    modified resultsets/relationships:
    
    - "contract_fraud_events":
    makes use of the fast billing.v_actual_billing_profiles
    view.
    
    - "billing_networks", "billing_profiles" and "contracts":
    have their billing_mappings relationship replaced by
    the billing_mappings_view resultset keep compatibility
    with existing panel code (ie. input check logic).
    
    - "contracts":
    the legacy "actual_billing_mappings" relationship is
    removed permanetly.
    
    resultset to remove:
    
    - "actual_billing_mappings":
    this legacy virtualview is only kept to be able to
    run the billing_mapping_effective_start_time.t.
    
    - "billing_mappings":
    not in use any more.
    
    Change-Id: Ica6d0e3f28873046047e0dc3636177a269fa4267

---
+++ lib/NGCP/Schema/Result/actual_billing_profiles.pm
@@ -0,0 +1,55 @@
+package NGCP::Schema::Result::actual_billing_profiles;
+
+use strict;
+use warnings;
+
+use Scalar::Util qw(blessed);
+use parent 'DBIx::Class::Core';
+
+our $VERSION = '2.007';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "Helper::Row::ToJSON");
+
+__PACKAGE__->table("billing.v_actual_billing_profiles");
+
+__PACKAGE__->add_columns(
+  "contract_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 0,
+  },
+  "billing_profile_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 0,
+  },
+);
+
+__PACKAGE__->set_primary_key("contract_id");
+
+__PACKAGE__->belongs_to(
+  "contract",
+  "NGCP::Schema::Result::contracts",
+  { id => "contract_id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+__PACKAGE__->belongs_to(
+  "billing_profile",
+  "NGCP::Schema::Result::billing_profiles",
+  { id => "contract_id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+sub TO_JSON {
+    my ($self) = @_;
+    return {
+        map { blessed($_) && $_->isa('DateTime') ? $_->datetime : $_ } %{ $self->next::method }
+    };
+}
+
+1;
