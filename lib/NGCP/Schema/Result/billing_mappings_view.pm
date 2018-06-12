b49882b4b2cc50574384056512cbb1b2d529c8a8
Rene Krenn <rkrenn@sipwise.com>
2018-06-12 16:28:09 +0200

    TT#38402 new billing mappings resultsets and relationships
    Change-Id: Ica6d0e3f28873046047e0dc3636177a269fa4267

---
+++ lib/NGCP/Schema/Result/billing_mappings_view.pm
@@ -0,0 +1,118 @@
+package NGCP::Schema::Result::billing_mappings_view;
+
+use strict;
+use warnings;
+
+use Scalar::Util qw(blessed);
+use parent 'DBIx::Class::Core';
+
+our $VERSION = '2.007';
+
+__PACKAGE__->load_components(
+  "InflateColumn::DateTime",
+  "Helper::Row::ToJSON",
+  "+NGCP::Schema::InflateColumn::DateTime::EpochMicro",
+);
+__PACKAGE__->table_class('DBIx::Class::ResultSource::View');
+__PACKAGE__->table("billing_mappings_view");
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "start_date",
+  {
+    data_type => "datetime",
+    datetime_undef_if_invalid => 1,
+    is_nullable => 1,
+  },
+  "end_date",
+  {
+    data_type => "datetime",
+    datetime_undef_if_invalid => 1,
+    is_nullable => 1,
+  },
+  "billing_profile_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 1,
+  },
+  "contract_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 0,
+  },
+  "network_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 1,
+  },
+);
+
+#__PACKAGE__->set_primary_key("id");
+
+__PACKAGE__->belongs_to(
+  "contract",
+  "NGCP::Schema::Result::contracts",
+  { id => "contract_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+__PACKAGE__->belongs_to(
+  "billing_profile",
+  "NGCP::Schema::Result::billing_profiles",
+  { id => "billing_profile_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "RESTRICT",
+    on_update     => "CASCADE",
+  },
+);
+
+__PACKAGE__->belongs_to(
+  "network",
+  "NGCP::Schema::Result::billing_networks",
+  { id => "network_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "RESTRICT",
+    on_update     => "CASCADE",
+  },
+);
+
+__PACKAGE__->result_source_instance->is_virtual(1);
+
+sub TO_JSON {
+    my ($self) = @_;
+    return {
+        map { blessed($_) && $_->isa('DateTime') ? $_->datetime : $_ } %{ $self->next::method }
+    };
+}
+
+__PACKAGE__->result_source_instance->view_definition(<<SQL);
+select
+  cbpn.grp as id,
+  cbps.contract_id as contract_id,
+  cbps.start_date as start_date,
+  cbps.end_date as end_date,
+  cbpn.billing_profile_id as billing_profile_id,
+  cbpn.billing_network_id as network_id
+from
+     billing.contracts_billing_profile_schedule cbps
+join billing.contracts_billing_profile_network cbpn on cbps.id = cbpn.schedule_id
+group by cbps.contract_id,cbpn.grp
+SQL
+
+1;
