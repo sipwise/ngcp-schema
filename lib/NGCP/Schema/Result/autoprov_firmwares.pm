92d82ac73951b275d66d02e74e39ae4be035fb55
Andreas Granig <agranig@sipwise.com>
2018-08-14 16:13:19 +0200

    TT#42767 Add tag column to autoprov firmwares table
    Change-Id: I4ba59ad26bba9e36711da1c70bec608375dde4a8

--- lib/NGCP/Schema/Result/autoprov_firmwares.pm
+++ lib/NGCP/Schema/Result/autoprov_firmwares.pm
@@ -31,6 +31,8 @@ __PACKAGE__->add_columns(
   { data_type => "varchar", is_nullable => 1, size => 255 },
   "filename",
   { data_type => "varchar", is_nullable => 1, size => 255 },
+  "tag",
+  { data_type => "varchar", is_nullable => 1, size => 255 },
 );
 
 __PACKAGE__->set_primary_key("id");
