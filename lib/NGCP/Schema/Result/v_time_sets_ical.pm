7f89d1b5137faa351384bd2dd0835b257dcfcdfe
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2019-01-18 02:26:18 +0100

    TT#49688 Generate ical format for events separately
    Change-Id: I2af483a11902ac5b574ee77d6affb6b9bd2f675f

--- lib/NGCP/Schema/Result/v_time_sets_ical.pm
+++ lib/NGCP/Schema/Result/v_time_sets_ical.pm
@@ -13,16 +13,38 @@ __PACKAGE__->table_class("DBIx::Class::ResultSource::View");
 
 
 __PACKAGE__->table("provisioning.v_time_sets_ical");
-__PACKAGE__->result_source_instance->view_definition("select `s`.`id` AS `id`,`s`.`name` AS `name`,concat('BEGIN:VCALENDAR\\n','PRODID:-//Mozilla.org/NONSGML Mozilla Calendar V1.1//EN\\n','VERSION:2.0\\n\\n',group_concat(concat('BEGIN:VEVENT\\n','UID:','sipwise',`p`.`id`,'\@sipwise',`s`.`id`,'\\n','SUMMARY:',`s`.`name`,' event ',`p`.`id`,'\\n','DTSTART:',date_format(`p`.`start`,'%Y%m%dT%H%i%s'),'\\n','RRULE:','FREQ=',`p`.`freq`,ifnull(concat(';COUNT=',`p`.`count`),''),ifnull(concat(';UNTIL=',date_format(`p`.`until`,'%Y%m%dT%H%i%s')),''),ifnull(concat(';INTERVAL=',`p`.`interval`),''),ifnull(concat(';BYSECOND=',`p`.`bysecond`),''),ifnull(concat(';BYMINUTE=',`p`.`byminute`),''),ifnull(concat(';BYHOUR=',`p`.`byhour`),''),ifnull(concat(';BYDAY=',`p`.`byday`),''),ifnull(concat(';BYMONTHDAY=',`p`.`bymonthday`),''),ifnull(concat(';BYYEARDAY=',`p`.`byyearday`),''),ifnull(concat(';BYWEEKNO=',`p`.`byweekno`),''),ifnull(concat(';BYMONTH=',`p`.`bymonth`),''),ifnull(concat(';BYSETPOS=',`p`.`bysetpos`),''),'\\n',ifnull(concat('DESCRIPTION:',`p`.`comment`,'\\n'),''),'END:VEVENT\\n') separator '\n'),'END:VCALENDAR\\n') AS `ical` from (`provisioning`.`voip_time_sets` `s` join `provisioning`.`voip_time_periods` `p` on((`s`.`id` = `p`.`time_set_id`))) group by `s`.`id`");
+__PACKAGE__->result_source_instance->view_definition("SELECT s.id, s.name,\\n",
+    "concat (
+        \"BEGIN:VCALENDAR\\n\",
+        \"PRODID:-//Mozilla.org/NONSGML Mozilla Calendar V1.1//EN\\n\",
+        IFNULL(CONCAT(\"NAME:\",s.name,\"\\n\"),''),
+        \"VERSION:2.0\\n\\n\",
+         GROUP_CONCAT(p.event_ical SEPARATOR '\\n'),
+        \"END:VCALENDAR\\n\"
+    ) AS ical
+FROM provisioning.voip_time_sets s LEFT JOIN v_time_periods_ical p ON s.id = p.time_set_id
+GROUP BY s.id");
 
 
 __PACKAGE__->add_columns(
   "id",
-  { data_type => "integer", extra => { unsigned => 1 }, is_nullable => 1 },
+  { data_type => "integer", extra => { unsigned => 1 }, is_nullable => 0 },
   "name",
   { data_type => "varchar", is_nullable => 0, size => 90 },
   "ical",
   { data_type => "text", is_nullable => 1 },
+);
+
+__PACKAGE__->has_many(
+  "periods_ical",
+  "NGCP::Schema::Result::v_time_periods_ical",
+  { "foreign.id" => "self.id" },
+);
+
+__PACKAGE__->belongs_to(
+  "voip_time_sets",
+  "NGCP::Schema::Result::v_time_periods_ical",
+  { "foreign.id" => "self.id" },
 );
 
 sub TO_JSON {
