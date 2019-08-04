b34b77c2ea38d0f4e1191ead5718002ab935c36d
Rene Krenn <rkrenn@sipwise.com>
2019-08-05 00:33:07 +0200

    TT#63545 rework contract_fraud_events
    Change-Id: Ie6800dee20f257cf6fc3e6e6b1b992dd9a05e731

--- lib/NGCP/Schema/Result/contract_fraud_events.pm
+++ lib/NGCP/Schema/Result/contract_fraud_events.pm
@@ -18,7 +18,7 @@ __PACKAGE__->table("contract_fraud_events");
 
 
 __PACKAGE__->add_columns(
-  "id",
+  "contract_id",
   {
     data_type => "integer",
     extra => { unsigned => 1 },
@@ -30,53 +30,46 @@ __PACKAGE__->add_columns(
     extra => { unsigned => 1 },
     is_nullable => 0,
   },
-  "interval",
+  "period",
   {
-    data_type => "varchar",
-    size      => 64,
+    data_type => "enum",
+    extra => { list => ["day", "month"] },
     is_nullable => 0,
   },
   "type",
   {
-    data_type => "varchar",
-    size      => 64,
-    is_nullable => 0,
+    data_type => "enum",
+    extra => { list => ["contract", "billing_profile"] },
+    is_nullable => 1,
   },
-  "interval_cost",
+  "customer_cost",
   {
     data_type => "decimal",
     size => [14,6],
     default_value => 0,
     is_nullable => 0,
   },
-  "interval_limit",
+  "reseller_cost",
   {
     data_type => "decimal",
     size => [14,6],
     default_value => 0,
     is_nullable => 0,
   },
-  "interval_lock",
+  "notify_status",
   {
-    data_type => "tinyint",
-    is_nullable => 1,
-    extra => { unsigned => 1 },
-  },
-  "interval_notify",
-  {
-    data_type => "varchar",
-    size      => 255,
-    is_nullable => 1,
-  },
-  "use_reseller_rates",
-  {
-    data_type => "tinyint",
+    data_type => "enum",
+    default_value => "new",
+    extra => { list => ["new", "notified"] },
     is_nullable => 0,
-    extra => { unsigned => 1 },
   },
-);
+  "notified_at",
+  {
+    data_type => "datetime",
+    is_nullable => 1,
+  },
 
-__PACKAGE__->set_primary_key("id");
+);
 
 sub TO_JSON {
     my ($self) = @_;
@@ -88,60 +81,49 @@ sub TO_JSON {
 __PACKAGE__->result_source_instance->is_virtual(1);
 
 __PACKAGE__->result_source_instance->view_definition(<<SQL);
-SELECT bpinfo.id, bpinfo.reseller_id, bpinfo.interval, bpinfo.type,
-       IF (bpinfo.fraud_use_reseller_rates > 0, SUM(cdr.source_reseller_cost),
-                                                SUM(cdr.source_customer_cost)) as interval_cost,
-       bpinfo.interval_limit,
-       bpinfo.interval_lock,
-       bpinfo.interval_notify,
-       bpinfo.fraud_use_reseller_rates as use_reseller_rates
-FROM (
-  SELECT c.id, n.reseller_id, bp.fraud_use_reseller_rates, i.interval,
-    IF (i.interval = 'month',
-        IF (cfp.fraud_interval_limit > 0,
-            'account_limit', 'profile_limit'),
-        IF (cfp.fraud_daily_limit > 0,
-            'account_limit', 'profile_limit')
-       ) AS type,
-    IF (i.interval = 'month',
-        IF (cfp.fraud_interval_limit > 0,
-            cfp.fraud_interval_limit, bp.fraud_interval_limit),
-        IF (cfp.fraud_daily_limit > 0,
-            cfp.fraud_daily_limit, bp.fraud_daily_limit)
-       ) AS interval_limit,
-    IF (i.interval = 'month',
-        IF (cfp.fraud_interval_limit > 0,
-            cfp.fraud_interval_lock, bp.fraud_interval_lock),
-        IF (cfp.fraud_daily_limit > 0,
-            cfp.fraud_daily_lock, bp.fraud_daily_lock)
-       ) AS interval_lock,
-    IF (i.interval = 'month',
-        IF (cfp.fraud_interval_limit > 0,
-            cfp.fraud_interval_notify, bp.fraud_interval_notify),
-        IF (cfp.fraud_daily_limit > 0,
-            cfp.fraud_daily_notify, bp.fraud_daily_notify)
-       ) AS interval_notify
-  FROM (SELECT IF(? = 'month','month','day') AS 'interval') i,
-       billing.contracts c
-  JOIN billing.v_actual_billing_profiles bp_actual ON bp_actual.contract_id = c.id
-  JOIN billing.billing_profiles bp ON bp.id = bp_actual.billing_profile_id
-  JOIN billing.contacts n ON n.id = c.contact_id
-  JOIN billing.resellers r ON r.id = n.reseller_id
-  LEFT JOIN billing.contract_fraud_preferences cfp ON cfp.contract_id = c.id
-  WHERE c.status = 'active'
-  HAVING interval_limit > 0
-) AS bpinfo
-JOIN accounting.cdr ON cdr.source_account_id = bpinfo.id
-WHERE CASE WHEN bpinfo.interval = 'month'
-  THEN cdr.start_time
-    BETWEEN UNIX_TIMESTAMP(DATE_FORMAT(NOW(), '%Y-%m-01 00:00:00'))
-        AND UNIX_TIMESTAMP(DATE_FORMAT(NOW() + INTERVAL 1 MONTH, '%Y-%m-01 00:00:00'))-1
-  ELSE cdr.start_time
-    BETWEEN UNIX_TIMESTAMP(DATE_FORMAT(NOW(), '%Y-%m-%d 00:00:00'))
-        AND UNIX_TIMESTAMP(DATE_FORMAT(NOW() + INTERVAL 1 DAY, '%Y-%m-%d 00:00:00'))-1
-  END
-GROUP BY bpinfo.id
-HAVING interval_cost >= interval_limit
+(select
+    c.id as contract_id,
+    contact.reseller_id as reseller_id,
+    cpc.period as period,
+    cpc.fraud_limit_type as type,
+    cpc.customer_cost as customer_cost,
+    cpc.reseller_cost as reseller_cost,
+    cpc.notified_status,
+    cpc.notified_at
+from
+     accounting.cdr_period_costs cpc
+join billing.contracts c on c.id = cpc.contract_id
+join billing.contacts contact on contact.id = c.contact_id
+
+where
+    cpc.period = 'month'
+and cpc.period_date = date_format(now(),'%Y-%m-01')
+and cpc.direction = 'out'
+and cpc.fraud_limit_exceeded = 1
+and c.status = 'active')
+
+union all
+
+(select
+    c.id as contract_id,
+    contact.reseller_id as reseller_id,
+    cpc.period as period,
+    cpc.fraud_limit_type as type,
+    cpc.customer_cost as customer_cost,
+    cpc.reseller_cost as reseller_cost
+    cpc.notified_status,
+    cpc.notified_at
+from
+     accounting.cdr_period_costs cpc
+join billing.contracts c on c.id = cpc.contract_id
+join billing.contacts contact on contact.id = c.contact_id
+
+where
+    cpc.period = 'month'
+and cpc.period_date = date_format(now(),'%Y-%m-01')
+and cpc.direction = 'out'
+and cpc.fraud_limit_exceeded = 1
+and c.status = 'active')
 SQL
 
 1;
