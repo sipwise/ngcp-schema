e9022630b0b81ab6b6e00ccd627ed50f83d45e9a
Rene Krenn <rkrenn@sipwise.com>
2020-04-16 17:24:27 +0200

    TT#80152 provisioning_templates resultset
    Change-Id: I937dde12495d2b56341acfd487c9f593dd4f719b

--- lib/NGCP/Schema/Result/resellers.pm
+++ lib/NGCP/Schema/Result/resellers.pm
@@ -232,6 +232,13 @@ __PACKAGE__->has_many(
   { cascade_copy => 0, cascade_delete => 0 },
 );
 
+__PACKAGE__->has_many(
+  "provisioning_templates",
+  "NGCP::Schema::Result::provisioning_templates",
+  { "foreign.reseller_id" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
 sub TO_JSON {
     my ($self) = @_;
     return {
