e3cf7dbc39023a1c0d875da020370befeb2f8ac6
Rene Krenn <rkrenn@sipwise.com>
2020-04-16 17:24:27 +0200

    TT#80152 provisioning_templates resultset
    Change-Id: I937dde12495d2b56341acfd487c9f593dd4f719b

---
+++ lib/NGCP/Schema/Result/provisioning_templates.pm
@@ -0,0 +1,84 @@
+package NGCP::Schema::Result::provisioning_templates;
+
+use strict;
+use warnings;
+
+use Scalar::Util qw(blessed);
+use parent 'DBIx::Class::Core';
+
+our $VERSION = '2.007';
+
+__PACKAGE__->load_components("InflateColumn::DateTime",
+    "Helper::Row::ToJSON",
+    "+NGCP::Schema::InflateColumn::DateTime::EpochMilli",);
+
+__PACKAGE__->table("billing.provisioning_templates");
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "reseller_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 1,
+  },
+  "name",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+  "description",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+  "lang",
+  {
+    data_type => "enum",
+    default_value => "js",
+    extra => { list => ["perl", "js"] },
+    is_nullable => 0,
+  },
+  "yaml",
+  { data_type => "text", is_nullable => 0 },
+  "modify_timestamp",
+  {
+    data_type => "timestamp",
+    datetime_undef_if_invalid => 1,
+    default_value => \"current_timestamp",
+    is_nullable => 0,
+  },
+  "create_timestamp",
+  {
+    data_type => "timestamp",
+    datetime_undef_if_invalid => 1,
+    default_value => "0000-00-00 00:00:00",
+    is_nullable => 0,
+  },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+__PACKAGE__->add_unique_constraint("resnam_idx", ["reseller_id", "name"]);
+
+__PACKAGE__->belongs_to(
+  "reseller",
+  "NGCP::Schema::Result::resellers",
+  { id => "reseller_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
+);
+
+sub TO_JSON {
+    my ($self) = @_;
+    return {
+        map { blessed($_) && $_->isa('DateTime') ? $_->datetime : $_ } %{ $self->next::method }
+    };
+}
+
+1;
