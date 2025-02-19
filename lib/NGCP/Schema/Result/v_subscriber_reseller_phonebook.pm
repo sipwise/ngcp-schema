6b6a40d4497128f3d942eca986febd4c0c2cd057
Kirill Solomko <ksolomko@sipwise.com>
2025-02-19 12:08:28 +0100

    MT#61805 add phonebook views schema definitions
    * v_contract_phonebook
    * v_contract_shared_phonebook
    * v_contract_reseller_phonebook
    * v_subscriber_phonebook
    * v_subscriber_contract_phonebook
    * v_subscriber_reseller_phonebook
    
    Change-Id: Iea5415ab66dd26f7ae560ffcd41fa6372d6a5c7d

---
+++ lib/NGCP/Schema/Result/v_subscriber_reseller_phonebook.pm
@@ -0,0 +1,123 @@
+package NGCP::Schema::Result::v_subscriber_reseller_phonebook;
+
+use strict;
+use warnings;
+
+use Scalar::Util qw(blessed);
+use parent 'DBIx::Class::Core';
+
+our $VERSION = '2.007';
+
+__PACKAGE__->table("billing.v_subscriber_reseller_phonebook");
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "varchar",
+    size => 256,
+    is_nullable => 0,
+  },
+  "subscriber_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 0,
+  },
+  "name",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+  "number",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+  "shared",
+  { data_type => "tinyint", default_value => 0, is_nullable => 0 },
+);
+
+__PACKAGE__->belongs_to(
+  "subscriber",
+  "NGCP::Schema::Result::voip_subscribers",
+  { id => "subscriber_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+1;
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+NGCP::Schema::Result::v_subscriber_reseller_phonebook
+
+=head1 DESCRIPTION
+
+This module is a schema class for the NGCP database table "billing.v_subscriber_reseller_phonebook".
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=back
+
+=head1 TABLE: C<billing.v_subscriber_reseller_phonebook>
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'varchar'
+  size: 256
+  is_nullable: 0
+
+=head2 contract_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 name
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 255
+
+=head2 number
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 255
+
+=head2 shared
+
+  data_type: 'tinyint'
+  is_nullable: 0
+  default_value: 0
+
+=head1 RELATIONS
+
+=head2 subscriber
+
+Type: belongs_to
+
+Related object: L<NGCP::Schema::Result::voip_subscribers>
+
+=head1 AUTHOR
+
+Sipwise Development Team C<< <support@sipwise.com> >>
+
+=head1 LICENSE
+
+This software is Copyright © 2018 by Sipwise GmbH, Austria.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this package.  If not, see <https://www.gnu.org/licenses/>.
